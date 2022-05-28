be8cc3095a6e4cce71688c5fab631209cbbc7872
Ndheeraj03 <raghudheeraj.03@gmail.com>
2022-05-28 21:59:56 +0530

    third commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	
+	int sum=0,a=0,b=0;
+	printf("\n my name is  : NARRA RAGHU DHEERAJ REDDY \n" );
+	printf("\n hello this my first program");
+	printf("\n iam trying write a simple addition program !!!!! help me out");
+	printf("\n please enter two numbers:");
+	scanf("%d %d",&a,&b);
+	printf("\n the sum is %d",(a+b));
+	printf("\n iam trying the job controls \n");
+	return 0;
+
+}
